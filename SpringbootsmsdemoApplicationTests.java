fb098061f8c19890df6e015e7bc19cb914be1900
kolagani ganesh <104778968+KOLAGANIGANESH@users.noreply.github.com>
2023-07-20 19:41:59 +0530

    Add files via upload

---
+++ SpringbootsmsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganesh.Springbootsmsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsmsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
